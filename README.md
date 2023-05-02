002ea7be55726d9703b537e0f60822d1d5cd8639
Nahid1911 <nahid.raihan@gmail.com>
2023-05-03 04:43:38 +0600

    practice complete

---
+++ README.md
@@ -0,0 +1,1 @@
+# project_with_webpack
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  
+};
